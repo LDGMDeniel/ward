8952456f433462f62d859533edbcb06a809528df
LDGMDeniel <den_mail@list.ru>
2015-09-25 20:07:26 +0500

    menu v1.03 dev

--- CRM/menu.php
+++ CRM/menu.php
@@ -1,12 +1,12 @@
 <?php
 $page=$_SERVER['REQUEST_URI'];
-$links=array('<a href="../index.php">TRS</a>',
-'<a href="index.php">CRM</a>',
-'<a href="list.php">Клиенты</a>',
-'<a href="list.php?t=man">Контакты</a>',
-'<a href="taskman.php">Задачи</a>',
-'<a href="info.php"><span id="t_gray">Карточка</span></a>',
-'<a href="../login.php?a=q">Выход</a>');
+$links=array('<a href="../index.php">TRS</a>',//0
+'<a href="index.php">CRM</a>',//1
+'<a href="list.php">Клиенты</a>',//2
+'<a href="list.php?t=man">Контакты</a>',//3
+'<a href="taskman.php">Задачи</a>',//4
+'<a href="info.php"><span id="t_gray">Карточка</span></a>',//5
+'<a href="../login.php?a=q">Выход</a>');//6
 switch($page){
  case '/index.php':$links[1]='CRM'; break;
  case '/CRM/list.php':$links[2]='Клиенты'; break;
@@ -17,10 +17,10 @@ switch($page){
     case '/CRM/task-zoom.php': $links[4]='Задачи'; break;
  case '/CRM/list.php?q=find':$links[5]='<span id="t_gray">Поиск</span>'; break;
  case '/CRM/create.php':$links[5]='<span id="t_gray">Создать</span>'; break;
- case '/info.php':$links[5]='<span id="t_gray">Карточка</span>'; break;
+ case '/CRM/info.php':$links[5]='<span id="t_gray">Карточка</span>'; break;
 }
 //каждый раз переписывать и эту хрень
-if(!isset($_SESSION['view'])){$links[6]='&nbsp;';}
+//if(!isset($_SESSION['view'])){$links[5]='&nbsp;';}
 if(!isset($_SESSION['name'])){$user='&nbsp;';}else{$user=$_SESSION['name'];}
 echo('<table cellspacing="0" rules="none" border="0" align="center"><tr>'."\r\n");
 echo('<td>'.$links[0]."</td>\r\n<td>".$links[1]."</td>\r\n<td>".$links[2].
