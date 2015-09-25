ebcaa995e9fa2a35254d5e1c95aa6ce5d6cc7d8e
LDGMDeniel <den_mail@list.ru>
2015-09-25 12:08:12 +0500

    reworking post

--- post.php
+++ post.php
@@ -21,81 +21,12 @@ if(isset($_SESSION['uid'])){
  -message
   newstatus* - при смене статуса заявки(*не реализовано)*/
  $post_result='';
+ //reworking
  if(isset($_POST['todo'])){
   switch($_POST['todo']){
    case 'message':
-    if(isset($_POST['newowner']) && isset($_POST['text']) && isset($_POST['type']) && isset($_POST['ticket']) && isset($_POST['tstatus'])){
-     $vi=array($_POST['newowner'], $_POST['text'], $_POST['type'], $_POST['ticket'], $_POST['tstatus']);
-     $va=validate_message_input_to_array($_POST['newowner'], $_POST['text'], $_POST['type'], $_POST['ticket'], $_POST['tstatus']);
-     if($va[0]==true){
-      $sqlresult=mysql_query('INSERT INTO `message` (`m_ticket`, `m_type`, `m_when`, `m_who`, `m_text`, `m_newowner`, `m_newstatus`)'.
-      ' VALUES ("'.$va[4].'", "'.$va[3].'", '.time().', "'.$_SESSION['uid'].'", "'.$va[2].'", "'.$va[1].'", "'.$va[5].'")',$dbc_sqlink);
-      if($sqlresult==false){
-       $post_result='Произошла ошибка MySQL. Ничего не сделано.';
-      }else{
-       switch ($va[3]){
-        case 'T':
-         if($va[1]==0){
-          $sqlresult=mysql_query('UPDATE `ticket` SET `t_owner` = "0", `t_status`="N"  WHERE `t_id` = "'.$va[4].'"',$dbc_sqlink);
-         }else{
-          $sqlresult=mysql_query('UPDATE `ticket` SET `t_owner` = "'.$va[1].'", `t_status`="'.$va[5].'" WHERE `t_id` = "'.$va[4].'"',$dbc_sqlink);
-         }
-         if($sqlresult==false){
-          $post_result='Произошла ошибка MySQL. Сообщение не повлияло на заявку.';
-         }
-         break;
-        case 'C':
-         $sqlresult=mysql_query('UPDATE `ticket` SET `t_status` = "C" WHERE `t_id` = "'.$va[4].'"',$dbc_sqlink);
-         if($sqlresult==false){
-          $post_result='Произошла ошибка MySQL. Сообщение не повлияло на заявку.';
-         }
-         break;
-       }
-      }
-     }else{
-      $post_result='Функция обработки сообщения вернула ошибку входных данных. Ничего не сделано.';
-     }
-    }else{
-     $post_result='Ожидания переменных не оправданы. Ничего не сделано.';
-    }
     break;
    case 'ticket':
-    if(isset($_POST['newowner']) && isset($_POST['text']) && isset($_POST['client']) && isset($_POST['caption'])){
-     if(isset($_POST['U'])){
-      if($_POST['U']=='yes'){
-       $newstatus='U';
-      }else{
-       $post_result='Обязательное значение вне ожиданий. Ничего не сделано.';
-      }
-     }else{
-      $newstatus='N';
-     }
-     if($post_result==''){
-      //ебаный костыль, я не мочь в логику, потому сначала готовим сообщение, потом постим тикет, потом меняем значение -1
-      $vm=validate_message_input_to_array($_POST['newowner'], $_POST['text'], 'N', -1, $newstatus);
-      $vt[0]=true;
-      $vt[1]=intval($_POST['client']);
-      if($vt[1]==0){$vt[0]=false;}
-      if(strlen($_POST['caption'])>50){$vt[0]=false;}
-      $vt[2]=str_replace('"','&quot;',$_POST['caption']);
-      if($vt[0] && $vm[0]){
-       $sqlresult=mysql_query('INSERT INTO `ticket` (`t_who`, `t_when`, `t_client`, `t_owner`, `t_caption`, `t_status`)
-       VALUES ("'.$_SESSION['uid'].'", '.time().', "'.$vt[1].'", "'.$vm[1].'", "'.$vt[2].'", "'.$vm[5].'")');
-       if($sqlresult==false){
-        $post_result='Произошла ошибка MySQL. Ничего не сделано.';
-       }else{
-        $vm[4]=mysql_insert_id();
-        $sqlresult=mysql_query('INSERT INTO `message` (`m_ticket`, `m_type`, `m_when`, `m_who`, `m_text`, `m_newowner`, `m_newstatus`)'.
-        ' VALUES ("'.$vm[4].'", "'.$vm[3].'", '.time().', "'.$_SESSION['uid'].'", "'.$vm[2].'", "'.$vm[1].'", "'.$vm[5].'")',$dbc_sqlink);
-        if($sqlresult==false){
-        $post_result='Произошла ошибка MySQL. Заявка может быть запорота.';
-        }
-       }
-      }else{
-       $post_result='Один из массивов не готов. Ничего не сделано.';
-      }
-     }
-    }
     break;
    default: $post_result='Форма прислала что-то не то. Ничего не сделано.';
   }
